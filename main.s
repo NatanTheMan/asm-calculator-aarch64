920e23827f6d2034b2fc01488978306bcae07575
Natan <natanscosta7@gmail.com>
2025-09-30 18:30:31 -0300

    create label to write on console

--- main.s
+++ main.s
@@ -1,9 +1,22 @@
 .global _start
 
 _start:
+	ldr x1, =msg
+	ldr x2, =msg_len
+	bl write
 	b exit
+
+write:
+	mov x8, #64
+	mov x0, 1
+	svc #0
+	ret
 
 exit:
 	mov x8, #93
 	mov x0, #0
 	svc #0
+
+msg:
+	.asciz "Hello\n"
+msg_len = . - msg
